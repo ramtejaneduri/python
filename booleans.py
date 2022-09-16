9543300cd8c35ae4e6ca50cf7313ff432b1926fe
ramtejaneduri <107628188+ramtejaneduri@users.noreply.github.com>
2022-09-16 08:01:25 +0530

    Add files via upload

---
+++ booleans.py
@@ -0,0 +1,13 @@
+print(10>9)
+print(10==9)
+print(5<5)
+a=500
+b=200
+if b>a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
+    print(bool("hello"))
+    print(bool("0"))
+    print(bool("-1"))
+
